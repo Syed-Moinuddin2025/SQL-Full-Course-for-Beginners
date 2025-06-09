8098f46e4339f013595fe11394d59946104aaf0a
Syed-Moinuddin2025 <s4moin123@gmail.com>
2025-06-09 17:12:37 +0530

    Uses `WHERE` clause to filter customers

--- 07_where_clause_filtering_customers.sql
+++ 07_where_clause_filtering_customers.sql
@@ -1,5 +1,6 @@
 USE MyDatabase
 
+
 SELECT * FROM customers
 
 -- Retrieve customers from Germany
@@ -7,16 +8,17 @@ SELECT *
 FROM customers
 WHERE country = 'Germany'
 
-
-
+-- Retrieve customers from India
 SELECT * 
 FROM customers
 WHERE country = 'India'
 
+-- Retrieve customers from UAE
 SELECT * 
 FROM customers 
 WHERE country = 'UAE'
 
+-- Retrieve customers from USA
 SELECT first_name , country 
 FROM customers
 WHERE country = 'USA'

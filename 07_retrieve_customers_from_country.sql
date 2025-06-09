3e0b665bb46f7a93d15deaef3a73b948b45000a4
Syed-Moinuddin2025 <s4moin123@gmail.com>
2025-06-09 14:56:38 +0530

    Retrieve customers filtered by country

---
+++ 07_retrieve_customers_from_country.sql
@@ -0,0 +1,22 @@
+USE MyDatabase
+
+SELECT * FROM customers
+
+-- Retrieve customers from Germany
+SELECT *
+FROM customers
+WHERE country = 'Germany'
+
+
+
+SELECT * 
+FROM customers
+WHERE country = 'India'
+
+SELECT * 
+FROM customers 
+WHERE country = 'UAE'
+
+SELECT first_name , country 
+FROM customers
+WHERE country = 'USA'
